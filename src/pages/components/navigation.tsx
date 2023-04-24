05eb4cf1fed0db318f2851e072a3a4e86344d0de
sch <sch102403@gmail.com>
2023-04-25 08:17:30 +0900

    fix: 각 컴포넌트 별 경로 명확하게 수정

--- src/pages/components/navigation.tsx
+++ src/pages/components/navigation.tsx
@@ -1,21 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/esm/Button';
-import Form from 'react-bootstrap/esm/Form';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-export default function Navigation() {
+const Navigation = () => {
   return (
     <>
       <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Flog</Navbar.Brand>
+        <Navbar.Brand href="/">Flog</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="category">Category</Nav.Link>
-            <Nav.Link href="tag">Tag</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/category">Category</Nav.Link>
+            <Nav.Link href="/tag">Tag</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -32,3 +32,5 @@ export default function Navigation() {
     </>
   )
 }
+
+export default Navigation;
