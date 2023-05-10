76fc4c33088b1314c0ff4a75b72aa63afab6d13d
sch <sch102403@gmail.com>
2023-05-10 18:35:17 +0900

    feat: 게시글에 코드 블록 구현

--- src/pages/post/[post].tsx
+++ src/pages/post/[post].tsx
@@ -1,10 +1,23 @@
 import Head from 'next/head';
 import { Col, Row } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 import Navigation from '../components/navigation';
 import Footer from '../components/footer';
+import { useEffect, useState } from 'react';
+
+import { markdown } from '@/dummyData';
 
 export default function Post() {
+  const [style, setStyle] = useState({});
+  useEffect(() => {
+    import('react-syntax-highlighter/dist/esm/styles/prism').then((mod) =>
+      setStyle(mod.default)
+    );
+  });
+
   return (
     <>
       <Head>
@@ -18,7 +31,28 @@ export default function Post() {
         <Row className="container mx-auto flex-fill">
           <Col md={2} className="d-none d-md-block"></Col>
           <Col md={8} className="flex-column">
-            <h3>Title</h3>
+            <ReactMarkdown
+              children={markdown}
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code({ node, inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || '');
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      {...props}
+                      children={String(children).replace(/\n$/, '')}
+                      style={style}
+                      language={match[1]}
+                      PreTag="div"
+                    />
+                  ) : (
+                    <code {...props} className={className}>
+                      {children}
+                    </code>
+                  );
+                },
+              }}
+            />
           </Col>
           <Col
             md={2}
