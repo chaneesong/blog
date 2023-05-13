6aa22d4584d3f6d7f671daecac9595832a2e91cc
sch <sch102403@gmail.com>
2023-05-14 02:56:24 +0900

    feat: redux 스토어 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css';
-import '@/styles/globals.css'
+import { Provider } from 'react-redux';
+
+import '@/styles/globals.css';
+import store from '@/store/store';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
