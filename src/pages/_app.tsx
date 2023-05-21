97b074d116cdd73e914b02f407831be0340ea9d0
sch <sch102403@gmail.com>
2023-05-22 05:56:42 +0900

    feat: 앱에 redux 연결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Provider } from 'react-redux';
 
 import '@/styles/globals.css';
-import wrapper from '@/redux';
+import { wrapper } from '@/redux';
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, ...pageProps }: AppProps) {
+  // store를 app과 연결 할 때 wrapper.useWrappedStore()를 사용하여 연결해야한다.
+  const { store, props } = wrapper.useWrappedStore(pageProps);
+  return (
+    <Provider store={store}>
+      <Component {...props.pageProps} />
+    </Provider>
+  );
 }
 
-export default wrapper.withRedux(App);
+export default App;
