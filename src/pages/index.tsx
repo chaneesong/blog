c9c87f1bd2c861b6712ca2a8798e9da066d2dab8
sch <sch102403@gmail.com>
2023-05-23 07:21:17 +0900

    fix: index page 내부 최근 게시글 더미데이터로 redux 연결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import Head from 'next/head';
-import MainContent from './components/mainContent';
 import { Row } from 'react-bootstrap';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks/reduxHooks';
+import { useEffect } from 'react';
+
 import AppLayout from './components/AppLayout';
+import MainContent from './components/mainContent';
+import { fetchRecentPostsThunk } from '@/redux/features/fetchRecentPosts/actions/postActions';
 
 export default function Home() {
+  const dispatch = useAppDispatch();
+  const posts = useAppSelector((state) => state.recentPosts.posts);
+
+  useEffect(() => {
+    dispatch(fetchRecentPostsThunk());
+  }, [dispatch]);
+
   return (
     <>
       <Head>
@@ -15,12 +26,9 @@ export default function Home() {
       <AppLayout>
         <h3>Latest Contents</h3>
         <Row className="justify-content-around">
-          <MainContent />
-          <MainContent />
-        </Row>
-        <Row className="justify-content-around">
-          <MainContent />
-          <MainContent />
+          {posts.map((post) => (
+            <MainContent key={post.id} post={post} />
+          ))}
         </Row>
       </AppLayout>
     </>
