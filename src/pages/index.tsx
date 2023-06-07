cef456071a63e02f339db5dae71d75d206b62bfb
chaneesong <sch102403@gmail.com>
2023-06-07 19:28:36 +0900

    fix: 기존 bootstrap PostCard 컴포넌트에서 커스텀 PostCard로 변경
    추후 화면이 좁아졌을 때 bootstrap PostCard 사용 예정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import Head from 'next/head';
 import { Row } from 'react-bootstrap';
-import { useAppDispatch, useAppSelector } from '@/redux/hooks/reduxHooks';
+import { useAppDispatch } from '@/redux/hooks/reduxHooks';
 import { useEffect } from 'react';
 
 import AppLayout from '../components/AppLayout';
-import RecentPostCard from '../components/RecentPostCard';
 import { fetchRecentPostsThunk } from '@/redux/features/fetchRecentPosts/actions/fetchRecentPostsActions';
+import { postData } from '../../public/dummyData';
+import styled from 'styled-components';
+import PostCard from '@/components/PostCard';
+
+const ContentsContainer = styled.div`
+  margin: 10px;
+  border: 1px solid white;
+`;
 
 export default function Home() {
   const dispatch = useAppDispatch();
-  const posts = useAppSelector((state) => state.postcards.data);
 
   useEffect(() => {
     dispatch(fetchRecentPostsThunk());
@@ -26,8 +32,8 @@ export default function Home() {
       <AppLayout>
         <h3>Latest Contents</h3>
         <Row className="justify-content-around">
-          {posts.map((post) => (
-            <RecentPostCard key={post.id} post={post} />
+          {postData.map((post) => (
+            <PostCard post={post} />
           ))}
         </Row>
       </AppLayout>
