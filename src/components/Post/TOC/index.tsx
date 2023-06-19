59725fe2abebe37c3be96c54ceb7fa5c57555e3a
chaneesong <sch102403@gmail.com>
2023-06-20 07:14:28 +0900

    feat: 게시글 목차 구현

--- src/components/Post/TOC/index.tsx
+++ src/components/Post/TOC/index.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+interface HeadingInfo {
+  text: string | null;
+  id: string;
+}
 
 const TOC = () => {
-  return <div>TOC Section</div>;
+  const [headingList, setHeadingList] = useState([] as HeadingInfo[]);
+
+  useEffect(() => {
+    setHeadingList(
+      Array.from(document.querySelectorAll('h1, h2, h3')).map(
+        (element, index): HeadingInfo => {
+          element.id = `heading-${index}`;
+          return { text: element.textContent, id: element.id };
+        }
+      )
+    );
+  }, []);
+
+  return (
+    <div>
+      {headingList.map((heading) => (
+        <Link href={`#${heading.id}`} key={heading.id}>
+          <div>{heading.text}</div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default TOC;
