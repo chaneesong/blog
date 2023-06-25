5b9c34fbfa9f1884dc11ff03050dbf5adea9fcab
chaneesong <sch102403@gmail.com>
2023-06-26 02:02:14 +0900

    더미데이터 변경으로 인한 데이터 수정

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -5,10 +5,10 @@ import { useAppDispatch, useAppSelector } from '@/redux/hooks/reduxHooks';
 
 import AppLayout from '@/components/AppLayout';
 import { setPost } from '@/redux/features/posterSlice/posterSlice';
-import { dummyPostData } from '../../../public/dummyData';
 import ContentContainer from '@/components/Post/Content';
 import Title from '@/components/Post/Title';
 import TOC from '@/components/Post/TOC';
+import { dummyPostData } from '../../../public/dummyData';
 
 const Post = () => {
   const router = useRouter();
@@ -21,7 +21,7 @@ const Post = () => {
   useEffect(() => {
     if (parseInt(id as string) !== postData.id) {
       // TODO 주소를 직접 입력하여 접근하는 경우 서버에 요청하여 데이터를 받아와야 함
-      const updatedPostData = { ...dummyPostData[3] };
+      const updatedPostData = { ...dummyPostData[1] };
       dispatch(setPost({ ...updatedPostData }));
     }
   }, []);
@@ -37,7 +37,7 @@ const Post = () => {
       <AppLayout>
         <Title titleProp={titleElement}></Title>
         <ContentContainer content={postData.content} />
-        <TOC></TOC>
+        <TOC />
       </AppLayout>
     </>
   );
