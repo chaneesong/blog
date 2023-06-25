fb3e3a18be0674f335178ba44678a4bf92323a8a
chaneesong <sch102403@gmail.com>
2023-06-26 06:11:26 +0900

    fix: 화면 크기에 따라 PostCard를 변화 시키기 위해 MiniPostCard 추가

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -4,6 +4,10 @@ import { useAppDispatch, useAppSelector } from '@/redux/hooks/reduxHooks';
 import AppLayout from '@/components/AppLayout';
 import PostCard from '@/components/PostCard';
 import { fetchPostsThunk } from '@/redux/features/fetchPosts/actions/fetchPostsAction';
+import MiniPostCard from '@/components/MiniPostCard';
+
+const MemoizedPostCard = React.memo(PostCard);
+const MemoizedMiniPostCard = React.memo(MiniPostCard);
 
 const PostList = () => {
   const dispatch = useAppDispatch();
@@ -13,14 +17,28 @@ const PostList = () => {
     dispatch(fetchPostsThunk());
   }, [dispatch]);
 
-  return (
-    <AppLayout>
-      <h2>All Posts</h2>
+  const separateLayers = (data: Post[]) => {
+    const postCards: JSX.Element[] = [];
+    const miniPostCards: JSX.Element[] = [];
 
-      {posts.map((post) => (
-        <PostCard key={post.id} post={post} />
-      ))}
-    </AppLayout>
+    data.forEach((post) => {
+      postCards.push(<MemoizedPostCard key={post.id} post={post} />);
+      miniPostCards.push(<MemoizedMiniPostCard key={post.id} post={post} />);
+    });
+
+    return { postCards, miniPostCards };
+  };
+
+  const { postCards, miniPostCards } = separateLayers(posts);
+
+  return (
+    <>
+      <AppLayout>
+        <h2>All Posts</h2>
+        <div>{postCards}</div>
+        <div className="row justify-content-around">{miniPostCards}</div>
+      </AppLayout>
+    </>
   );
 };
 
