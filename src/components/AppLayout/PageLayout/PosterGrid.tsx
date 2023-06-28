4f34079cd64158bd85d94805ccd52baf1766ff6e
chaneesong <sch102403@gmail.com>
2023-06-28 18:00:06 +0900

    style: 게시글 레이아웃 변경

--- src/components/AppLayout/PageLayout/PosterGrid.tsx
+++ src/components/AppLayout/PageLayout/PosterGrid.tsx
@@ -4,17 +4,19 @@ const PosterGrid = ({ children }: ReactNodeProps) => {
   const [title, content, toc] = React.Children.toArray(children);
 
   return (
-    <div className="container mx-auto flex-fill contents-padding-top">
-      <div className="row">
-        <div className="col-md-1"></div>
-        <div className="col-md-8">{title}</div>
+    <>
+      <div className="container mx-auto flex-fill contents-padding-top">
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-8">{title}</div>
+        </div>
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="flex-column col-md-8">{content}</div>
+          <div className="col-md-3">{toc}</div>
+        </div>
       </div>
-      <div className="row">
-        <div className="col-md-1"></div>
-        <div className="flex-column col-md-8">{content}</div>
-        <div className="col-md-3">{toc}</div>
-      </div>
-    </div>
+    </>
   );
 };
 
