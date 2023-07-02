322d0ce835722b5d5a86d89ed70ff32639f87482
chaneesong <sch102403@gmail.com>
2023-07-03 00:01:42 +0900

    style: footer 위치 변경 및 파일 분리

---
+++ src/components/AppLayout/Footer/styled.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const AppFooter = styled.footer`
+  background-color: #efefef;
+  width: 100%;
+`;
+
+export const Image = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-left: 10px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const Copyright = styled.span`
+  color: grey;
+`;
