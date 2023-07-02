6a1d0aad79f49156b6f42c9ce368c7614074e2c4
chaneesong <sch102403@gmail.com>
2023-07-02 23:57:46 +0900

    style: footer style 추가

--- src/components/AppLayout/Footer.tsx
+++ src/components/AppLayout/Footer.tsx
@@ -2,14 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 
 const AppFooter = styled.footer`
+  background-color: #efefef;
   width: 100%;
-  border-top: solid;
+`;
+
+const Image = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-left: 10px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
 `;
 
 const Footer = () => {
   return (
     <AppFooter>
-      <div className="container p-3">© 2023 Chaneesong Copyright</div>
+      <Container className="container p-4">
+        <span>© 2023 Chaneesong Copyright</span>
+        <div>
+          <a href="https://github.com/chaneesong">
+            <Image src="/images/github-icon.png" alt="github" />
+          </a>
+          <a href="mailto:sch102403@gmail.com">
+            <Image src="/images/mail.png" alt="mail" />
+          </a>
+        </div>
+      </Container>
     </AppFooter>
   );
 };
