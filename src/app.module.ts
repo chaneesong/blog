22d0dadc54ad3bcc818f20cb6e3ff690443e8ffc
chaneesong <sch102403@gmail.com>
2023-08-03 06:55:01 +0900

    feat: mysql 연결 및 dotenv 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: process.env.MYSQL_PASSWORD,
+      database: 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      // 서버가 재시작 할 때 DB를 초기화하는 역할, 프로덕션에서는 false로 변경
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
