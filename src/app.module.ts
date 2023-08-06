9982dad22509437865720d58180526e690695e7e
chaneesong <sch102403@gmail.com>
2023-08-07 03:07:06 +0900

    feat: 새로 생성한 모듈 추가 및 database 명 환경변수로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryModule } from './category/category.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
   imports: [
@@ -12,12 +14,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       port: 3306,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
-      database: 'test',
+      database: process.env.DATABASE_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       // 서버가 재시작 할 때 DB를 초기화하는 역할, 프로덕션에서는 false로 변경
       synchronize: process.env.DEVELOPMENT === 'true',
       verboseRetryLog: process.env.DEVELOPMENT === 'true',
     }),
+    CategoryModule,
+    TagModule,
   ],
   controllers: [],
   providers: [],
