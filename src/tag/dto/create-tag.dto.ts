bae8a6b54145f182b2163b5bdf5e8e280e632ff3
chaneesong <sch102403@gmail.com>
2023-08-10 07:07:12 +0900

    feat: tags를 등록하는 서비스 로직 구현

--- src/tag/dto/create-tag.dto.ts
+++ src/tag/dto/create-tag.dto.ts
@@ -1,1 +1,7 @@
-export class CreateTagDto {}
+import { IsArray, IsString } from 'class-validator';
+
+export class CreateTagDto {
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+}
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from './entities/tag.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TagService {
-  create(createTagDto: CreateTagDto) {
-    return 'This action adds a new tag';
+  constructor(
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
+  ) {}
+
+  async create(createTagDto: CreateTagDto) {
+    const { tags } = createTagDto;
+    const result: string[] = [];
+
+    for (const tag of tags) {
+      let existingTag = await this.tagRepository.findOne({ where: { tag } });
+
+      if (!existingTag) {
+        const newTag = this.tagRepository.create({ tag });
+        existingTag = await this.tagRepository.save(newTag);
+      }
+
+      result.push(existingTag.id);
+    }
+    return result;
   }
 
   findAll() {
