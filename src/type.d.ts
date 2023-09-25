bcff35acf2299a6eb37a6bddff1d25a32443e44c
chaneesong <sch102403@gmail.com>
2023-09-25 09:45:25 +0900

    feat: 글로벌 타입 재정의

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,12 +1,20 @@
-type Category = string;
+type Category = {
+  id: string;
+  keyword: string;
+};
 
-type Tag = string;
+type Tag = {
+  id: string;
+  keyword: string;
+};
 
 interface Post {
   id: number;
   title: string;
   content: string;
-  createdAt: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
   category: Category;
   tags: Tag[];
 }
