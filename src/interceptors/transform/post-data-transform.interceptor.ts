3309cdd91f5b77e514fbd07bf1d2ec8c79117d74
chaneesong <sch102403@gmail.com>
2023-10-08 14:03:32 +0900

    feat: 날짜 데이터 요청에 맞게 변경하는 로직 추가

--- src/interceptors/transform/post-data-transform.interceptor.ts
+++ src/interceptors/transform/post-data-transform.interceptor.ts
@@ -8,6 +8,12 @@ import { Observable, map } from 'rxjs';
 
 @Injectable()
 export class PostDataTransformInterceptor implements NestInterceptor {
+  private KR_TIME: number;
+
+  constructor() {
+    this.KR_TIME = 9 * 60 * 60;
+  }
+
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
@@ -23,11 +29,12 @@ export class PostDataTransformInterceptor implements NestInterceptor {
   }
 
   private dataTransform(data: PostRawData): Post {
+    console.log(data);
     return {
       id: data.post_id,
       title: data.post_title,
       content: data.post_content,
-      createdAt: data.post_createdAt,
+      createdAt: this.dateTransform(new Date(data.post_createdAt)),
       category: {
         id: data.category_id,
         keyword: data.category_keyword,
@@ -47,5 +54,10 @@ export class PostDataTransformInterceptor implements NestInterceptor {
     return tagIds.map(
       (tag, idx): Tag => ({ id: tag, keyword: tagKeywords[idx] }),
     );
+  }
+
+  private dateTransform(createdAt: Date): string {
+    const KRcreatedAt = new Date(createdAt.getTime() + this.KR_TIME);
+    return KRcreatedAt.toISOString().split('T')[0];
   }
 }
