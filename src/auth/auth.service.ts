c09c0992f5ee1cf10af4f3b9e41399ff7b314d3e
chaneesong <sch102403@gmail.com>
2023-10-09 15:50:21 +0900

    feat: 인증 로직 구현

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  private userId: string;
+  private hash: string;
+  constructor() {
+    this.userId = process.env.USER_ID;
+    this.hash = process.env.USER_HASH;
+  }
+
+  async signIn(userId: string, pass: string) {
+    const result = bcrypt.compare(pass, this.hash);
+    if (!result || userId !== this.userId) {
+      throw new UnauthorizedException();
+    }
+    return userId;
+  }
+}
