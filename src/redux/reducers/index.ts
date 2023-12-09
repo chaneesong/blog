b9d09627bbe2a3cb748cfe133bb2efe9318453cf
chaneesong <sch102403@gmail.com>
2023-12-10 03:17:09 +0900

    fix: url 수정

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -10,7 +10,7 @@ import { fetchPostsReducer } from '../features/fetchPosts/fetchPostsSlice';
 import { posterReducer } from '../features/fetchPost/posterSlice';
 import axios from 'axios';
 
-const backURL = 'http://api.chaneesong.xyz:3000';
+const backURL = 'api.chaneesong.xyz:3000';
 
 axios.defaults.baseURL = `https://${backURL}`;
 axios.defaults.withCredentials = true;
