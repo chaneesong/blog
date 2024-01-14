ba08ebc00c01eecf6d8c9ede8a278aa1a91cd2dc
chaneesong <sch102403@gmail.com>
2024-01-14 21:29:00 +0900

    feat: google 인증 방식 OAuth2에서 JWT로 변경

---
+++ .github/workflows/send-request/googleDrive/getGoogleAccessToken.js
@@ -0,0 +1,19 @@
+import { google } from 'googleapis';
+
+// 구글 드라이브 접근을 위해 OAuth2 인증을 하는 함수
+export const getGoogleAccessToken = async (googleDriveAPICredentials) => {
+  const credentials = JSON.parse(googleDriveAPICredentials);
+  const auth = new google.auth.JWT(
+    credentials.client_email,
+    null,
+    credentials.priavate_key,
+    ['https://www.googleapis.com/auth/drive.file']
+  );
+
+  return auth;
+};
+
+// 구글 드라이브 접근 권한을 얻는 함수
+export const getDriveAccess = (auth) => {
+  return google.drive({ version: 'v3', auth });
+};
