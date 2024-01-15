f106c78922562f1b1b35cd931c1362fa92951729
chaneesong <sch102403@gmail.com>
2024-01-15 11:22:11 +0900

    feat: 구글 드라이브 환경변수 변경으로 인한 구글 드라이브 인증에 관한 변수 수정

--- .github/workflows/send-request/googleDrive/uploadImage.js
+++ .github/workflows/send-request/googleDrive/uploadImage.js
@@ -1,5 +1,8 @@
 import { createReadStream, existsSync } from 'node:fs';
-import { getOAuth2Client, getDriveAccess } from './getOAuth2.js';
+import {
+  getOAuth2Client,
+  getGoogleAccessToken,
+} from './getGoogleAccessToken.js';
 import { createFolder } from './createFolder.js';
 import { parseImageName } from './parseImageName.js';
 import { getImageName } from './getImageName.js';
@@ -36,19 +39,14 @@ const uploadImageToDrive = async (drive, imagePath, folderId) => {
 
 // 이미지를 업로드 하는 함수 로직
 export const uploadImage = async (fileName) => {
-  const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL } =
-    process.env;
+  const googleDriveAPICredentials = process.env.GOOGLE_DRIVE_API_CREDENTIALS;
   const imgPath = fileName.split('.')[0];
 
   if (!existsSync(imgPath)) return null;
 
   try {
-    const oauth2Client = await getOAuth2Client(
-      GOOGLE_CLIENT_ID,
-      GOOGLE_CLIENT_SECRET,
-      GOOGLE_REDIRECT_URL
-    );
-    const drive = getDriveAccess(oauth2Client);
+    const auth = await getGoogleAccessToken(googleDriveAPICredentials);
+    const drive = getDriveAccess(auth);
     const folderId = await createFolder(drive, imgPath);
     const imgIds = await uploadImageToDrive(drive, imgPath, folderId);
     return imgIds;
