0b6664ddbdf59281aa31881890cc0d4f2e18d8f6
chaneesong <sch102403@gmail.com>
2024-01-16 12:27:38 +0900

    불필요한 코드 제거

--- .github/workflows/send-request/googleDrive/uploadImage.js
+++ .github/workflows/send-request/googleDrive/uploadImage.js
@@ -1,8 +1,5 @@
 import { createReadStream, existsSync } from 'node:fs';
-import {
-  getOAuth2Client,
-  getGoogleAccessToken,
-} from './getGoogleAccessToken.js';
+import { getGoogleAccessToken } from './getGoogleAccessToken.js';
 import { createFolder } from './createFolder.js';
 import { parseImageName } from './parseImageName.js';
 import { getImageName } from './getImageName.js';
