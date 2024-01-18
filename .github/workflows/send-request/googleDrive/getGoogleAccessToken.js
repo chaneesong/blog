38501ca4a33905cb4be57dc28be95009a297b25c
chaneesong <sch102403@gmail.com>
2024-01-18 10:09:00 +0900

    test

--- .github/workflows/send-request/googleDrive/getGoogleAccessToken.js
+++ .github/workflows/send-request/googleDrive/getGoogleAccessToken.js
@@ -2,7 +2,8 @@ import { google } from 'googleapis';
 
 // 구글 드라이브 접근을 위해 OAuth2 인증을 하는 함수
 export const getGoogleAccessToken = async (googleDriveAPICredentials) => {
-  const credentials = JSON.parse(googleDriveAPICredentials);
+  // const credentials = JSON.parse(googleDriveAPICredentials);
+  const credentials = JSON.parse(process.env.GOOGLE_DRIVE_API_CREDENTIALS);
   const auth = new google.auth.JWT(
     credentials.client_email,
     null,
