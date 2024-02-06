5cbc24f932bea1633f22e561d4f74d190a8f406b
chaneesong <sch102403@gmail.com>
2024-02-06 17:55:54 +0900

    posts 레포지토리 분리로 인한 삭제

--- .github/workflows/send-request/googleDrive/getImageName.js
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-
-// 디렉토리 내부에서 이미지명을 받아오는 함수
-// 이미지를 보관하는 디렉토리 이름은 파일명과 같다.
-export const getImageName = (imagePath) => {
-  const dir = fs.readdirSync(`${process.cwd()}/${imagePath}`);
-  console.log('dir', dir);
-  return dir;
-};
