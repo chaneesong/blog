436ae064d343b0b3585ef5d8954f3f20c7bf71bd
chaneesong <sch102403@gmail.com>
2024-02-20 17:38:00 +0900

    compose 명령어 수정

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ IMAGE_NAME=$3
 DOPPLER_TOKEN=$4
 
 cd $TARGET_DIR
-docker-compose down $SERVICE --volumes --rmi all
+docker compose down $SERVICE --volumes --rmi all
 docker pull ghcr.io/chaneesong/$IMAGE_NAME:latest
 $DOPPLER_TOKEN docker compose up -d api
