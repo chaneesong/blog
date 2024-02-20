093187c88bee1fe71c9f50ac96375f94eed71cb2
chaneesong <sch102403@gmail.com>
2024-02-20 18:46:09 +0900

    변수 수정

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker compose down $SERVICE --volumes --rmi all
 docker pull ghcr.io/chaneesong/$IMAGE_NAME:latest
 $($DOPPLER_TOKEN) docker compose up -d $SERVICE
 if [ "$SERVICE" = "api" ]; then
-  DOPPLER_TOKEN_API='$(DOPPLER_TOKEN)' docker compose up -d $SERVICE
+  DOPPLER_TOKEN_API=$(DOPPLER_TOKEN) docker compose up -d $SERVICE
 else
-  DOPPLER_TOKEN_WEB='$(DOPPLER_TOKEN)' docker compose up -d $SERVICE
+  DOPPLER_TOKEN_WEB=$(DOPPLER_TOKEN) docker compose up -d $SERVICE
 fi
