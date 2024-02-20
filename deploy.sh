18332e429d7591be4e849cdf2fc46269327a0da2
chaneesong <sch102403@gmail.com>
2024-02-20 18:59:21 +0900

    doppler token 수정

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,12 @@
 TARGET_DIR=$1
 SERVICE=$2
 IMAGE_NAME=$3
-DOPPLER_TOKEN=$4
 
 cd $TARGET_DIR
 docker compose down $SERVICE --volumes --rmi all
 docker pull ghcr.io/chaneesong/$IMAGE_NAME:latest
 if [ "$SERVICE" = "api" ]; then
-  DOPPLER_TOKEN_API='$(DOPPLER_TOKEN)' docker compose up -d $SERVICE
+  DOPPLER_TOKEN_API="$(doppler configs tokens create --project blog --config prd_api api-prd-token --plain --max-age 1m)" docker compose up -d $SERVICE
 else
-  DOPPLER_TOKEN_WEB='$(DOPPLER_TOKEN)' docker compose up -d $SERVICE
+  DOPPLER_TOKEN_WEB="$(doppler configs tokens create --project blog --config prd_web web-prd-token --plain --max-age 1m)" docker compose up -d $SERVICE
 fi
